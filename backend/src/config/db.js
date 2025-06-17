1d712d6ef83575bbe8b9dffb699e67309d370390
smartpools <rherrera@smartpools.pe>
2025-06-17 07:12:22 -0500

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 connection.connect((err) => {
@@ -16,4 +19,4 @@ connection.connect((err) => {
   console.log("✅ Conexión a la base de datos MySQL exitosa");
 });
 
-module.exports = connection;
+module.exports = Pool;
